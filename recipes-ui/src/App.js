fa0feb91797b8968a104412293977714843ff79d
ortizfe <79375454+ortizfe@users.noreply.github.com>
2023-02-22 19:00:13 -0500

    generic card added

--- recipes-ui/src/App.js
+++ recipes-ui/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>This will be the header</p>
+      <p>Then I will have the main container</p>
+      <p>I can add a footer why not</p>
     </div>
   );
 }

1f953becd32e079728b8eda3e17523a511eca4ee
ortizfe <79375454+ortizfe@users.noreply.github.com>
2023-03-03 18:03:30 -0500

    updated app.js

--- recipes-ui/src/App.js
+++ recipes-ui/src/App.js
@@ -1,28 +1,9 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Navigate,
-  Routes,
-} from "react-router-dom";
-
-import MainNavigation from "./shared/components/navigation/MainNavigation";
 import "./App.css";
 
 const App = () => {
-  const routes = (
-    <Routes>
-      <Route path="/" exact></Route>
-      <Route path="/addrecipes" exact></Route>
-      <Navigate to="/" />
-    </Routes>
-  );
   return (
     <div>
       <p>Header</p>
-      <Router>
-        <MainNavigation />
-        <main>{routes}</main>
-      </Router>
     </div>
   );
 };
--- recipes-ui/src/shared/components/ui-elements/Card.js
+++ recipes-ui/src/shared/components/ui-elements/Card.js
@@ -1,11 +1,7 @@
 import "./Card.css";
 
 const Card = (props) => {
-  return (
-    <div className={`card ${props.className}`} style={props.style}>
-      {props.children};
-    </div>
-  );
+  return <div className={`card ${props.className}`}>{props.children};</div>;
 };
 
 export default Card;
